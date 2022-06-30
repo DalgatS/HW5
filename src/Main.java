802a3b0304be04c249236d50e8da88d61bd75dfc
Dalgat <selimovi@mail.ru>
2022-06-30 16:32:36 +0300

    HW6 complete re2

--- src/Main.java
+++ src/Main.java
@@ -20,47 +20,55 @@ public class Main {
         double[] osn = {1.57, 7.654, 9.986};
         int[] ott2 = {1, 2, 3, 4, 5, 6};
 
-        for (int i = 0; i < ott.length; i++)
+        for (int i = 0; i < ott.length; i++) {
             System.out.print(ott[i]);
+        }
         System.out.println();
 
-        for (int k = 0; k < osn.length; k++)
-            System.out.print(osn[k] + " ");
+        for (int i = 0; i < osn.length; i++) {
+            System.out.print(osn[i] + " ");
+        }
         System.out.println();
 
-        for (int l = 0; l < ott2.length; l++)
-            System.out.print(ott2[l]);
-
-        System.out.println();
-
-
-        for (int z = (ott.length - 1); z >= 0; z--)
-            System.out.print(ott[z]);
-        System.out.println();
-
-        for (int d = (osn.length - 1); d >= 0; d--)
-            System.out.print(osn[d] + " ");
-        System.out.println();
-
-        for (int r = (ott2.length - 1); r >= 0; r--)
-            System.out.print(ott2[r]);
-
+        for (int i = 0; i < ott2.length; i++) {
+            System.out.print(ott2[i]);
+        }
 
         System.out.println();
 
-        for (int x = 0; x < ott2.length; x++)
-            if (ott2[x] % 2 != 0) {
-                System.out.print(ott2[x] + 1);
-            } else System.out.print(ott2[x]);
 
+        for (int i = (ott.length - 1); i >= 0; i--) {
+            System.out.print(ott[i]);
+        }
+        System.out.println();
+
+        for (int i = (osn.length - 1); i >= 0; i--) {
+            System.out.print(osn[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = (ott2.length - 1); i >= 0; i--) {
+            System.out.print(ott2[i]);
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < ott2.length; i++) {
+            if (ott2[i] % 2 != 0) {
+                System.out.print(ott2[i] + 1);
+            } else {
+                System.out.print(ott2[i]);
+            }
+        }
     }
 
     public static void hw6() {
 
         int[] arr = generateRandomArray();
 
-        for (int i = 0; i < arr.length; i++)
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
+        }
         System.out.println();
 
 
@@ -72,34 +80,31 @@ public class Main {
         System.out.println();
 
 
-        int max = 0;
+        int max = -1;
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < max) ;
-            {
+            if (arr[i] > max) {
                 max = arr[i];
             }
         }
         System.out.print("Максимальная сумма трат за день составила " + max + " рублей");
         System.out.println();
 
-        int min = 0;
+        int min = 200000;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > min) ;
-            {
+            if (arr[i] < min) {
                 min = arr[i];
             }
         }
         System.out.print("Минимальная сумма трат за день составила " + min + " рублей");
         System.out.println();
 
-
-        double average = (sum / arr.length);
+        double sum2 = sum;
+        double average = (sum2 / arr.length);
         System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
 
 
-
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = (reverseFullName.length - 1); i >= 0; i--)
             System.out.print(reverseFullName[i]);
         System.out.println();
